aa310c5f12e73fb51dc204df2c2b620dc7c2ce23
agatarigan <aqfalagabimantatarigan@gmail.com>
2025-07-23 17:52:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleFAQ(element) {
+  const answer = element.nextElementSibling;
+  answer.classList.toggle('active');
+}
+
+function processPayment() {
+  alert('Fitur pembayaran belum tersedia.');
+}
